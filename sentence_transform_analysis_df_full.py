f71615b86c41fa2b04889fc6c601354f7c995a8c
Andrew Kinard <akinard@cisco.com>
2024-04-01 13:02:07 -0400

    copyright notice

--- sentence_transform_analysis_df_full.py
+++ sentence_transform_analysis_df_full.py
@@ -1,5 +1,30 @@
 #!/usr/bin/env python
 
+"""
+This script performs sentence transform cosine analysis on libraries of patristic texts.
+
+Copyright (c) 2024  Andrew Kinard
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+to permit persons to whom the Software is furnished to do so, subject to the
+following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+
+
 # TODO: add any needed sources to the Palamas_sources and the Patristic_sources directories
 
 # Requirements
